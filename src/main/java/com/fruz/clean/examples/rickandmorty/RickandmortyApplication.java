ff46dc2645c2e7caa85e87389a2759676972c27b
Felipe Ruz <feliperuzg@gmail.com>
2019-08-25 15:41:29 -0400

    initial commit

---
+++ src/main/java/com/fruz/clean/examples/rickandmorty/RickandmortyApplication.java
@@ -0,0 +1,13 @@
+package com.fruz.clean.examples.rickandmorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickandmortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickandmortyApplication.class, args);
+	}
+
+}
